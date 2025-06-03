076673fd657a268dbab333abfa43958be51bc7cd
martinwolf-create <wolfmartin707@gmail.com>
2025-06-03 15:51:40 +0200

    first commit

---
+++ main.dart
@@ -0,0 +1,16 @@
+void main() {
+  String gdStr = "Guten";
+  String dayStr = "Tag";
+  String worldStr = "Welt";
+  int d = 20;
+  String timeStr = "Zeit";
+  int seven = 7;
+  int tweleve = 12;
+  String b = "!";
+  int a = 24;
+  String c = "Schöne";
+  String morning = "Morgen";
+  String s = "Heute";
+  String spa = " ";
+  print(c);
+}
